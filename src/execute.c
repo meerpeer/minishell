d5538573b239b65a985ff26f14156ca9ce2099d5
Merel Van der wal <mevan-de@f1r6s17.codam.nl>
2022-10-11 11:32:24 +0200

    added a pseudocode execute file

---
+++ src/execute.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   execute.c                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mevan-de <mevan-de@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/10/11 11:24:35 by mevan-de      #+#    #+#                 */
+/*   Updated: 2022/10/11 11:31:20 by mevan-de      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+//child_process
+// - open all in
+// - open all out
+// redirect in
+// redirect out
+// close all the fd boys
+// execve this beauty
+
+//execute
+//check if build in
+//pipe and fork :)
+//if( child)
+//	child_process
+//if (parent)
+//	close pipe ends?
+//	don't close STDIN/ STDOUT
---
+++ src/redirection.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   redirection.c                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mevan-de <mevan-de@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/10/11 11:24:32 by mevan-de      #+#    #+#                 */
+/*   Updated: 2022/10/11 11:27:57 by mevan-de      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+//open in files
+//every command can have multiple in files, try to open them all WRONLY
+//here_doc is also a thing here...
+//exit child process on fail
+
+//open out files
+//every command can have multiple out files
+//exit on fail
+
+//redirect in files
+//check re
+
+//redirect out files
+
+//close all fds
+void	close_unused_fds()
+{
+	return ;
+}
