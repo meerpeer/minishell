ba91746a653c83ed10eb4a6cba1dd02db7d7da28
Merel van der Wal <Merelvanderwal94@gmail.com>
2022-11-11 09:57:34 +0100

    Revert "merge changes accepted-smile-"
    This reverts commit e2287f4f6e29b1c161bd91251fe564ba95fc4863, reversing
    changes made to 17b4e0a064fd6d144be8ea407bcbe72cc6403263.

--- src/prompt.c
+++ src/prompt.c
@@ -6,7 +6,7 @@
 /*   By: merel <merel@student.42.fr>                  +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/11 07:48:55 by lhoukes       #+#    #+#                 */
-/*   Updated: 2022/11/10 13:54:55 by lhoukes       ########   odam.nl         */
+/*   Updated: 2022/11/10 13:27:11 by lhoukes       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,7 @@
 void	handle_signals(void);
 void	set_signals_noninteractive(void);
 
-//void	prompt_loop(t_mini *mini_data)
-void	prompt_loop(t_mini *mini_data, char *input_outside)
+void	prompt_loop(t_mini *mini_data)
 {
 	char	*input;
 	
@@ -27,27 +26,21 @@ void	prompt_loop(t_mini *mini_data, char *input_outside)
 		input = readline(Y185"🐌mini🐚$ "RESET);
 		//rl_catch_sig
 		//set_signals_noninteractive();
-		if (input_outside) 
-			input = input_outside;
-		else
-			input = readline(Y185"🐌mini🐚$ "RESET);
 		if (input == NULL)
 			error_exit("Readline fail", NULL, NULL, 1);
 		if (input)
 		{
 			add_history(input);
 			mini_data->cmd_input = input;
-			lexer(mini_data);	
+			lexer(mini_data);
 			if (try_parsing(mini_data))
 			{
-				print_cmds(mini_data);
+				//print_cmds(mini_data);
 				execute_cmds(mini_data);
 			}
-			ft_lstclear(&mini_data->tokens, delete_token_list);
-			if (input_outside)
-				exit (mini_data->exit_status);
-			reset_mini_data(mini_data);
 			free(input);
+			ft_lstclear(&mini_data->tokens, delete_token_list);
+			reset_mini_data(mini_data);
 		}
 	}
 }
